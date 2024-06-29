071b5ce1cad99c37c37a3a32b91e793fc5c23252
jonell888 <144983408+jonell888@users.noreply.github.com>
2024-06-29 23:03:51 +0700

    add new

---
+++ file upload/Example.js
@@ -0,0 +1,13 @@
+let upload = document.getElementById("upload")
+let image = document.getElementById("image")
+
+upload.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const render = new FileReader();
+        render.upload = (e) => {
+            image.src = e.tarlget.result;
+        };
+        render.readAsDataURL(file);
+    }
+})
