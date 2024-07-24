639e85df01effd673921830d389e869f63b438e8
jonell888 <643070310021@npu.ac.th>
2024-07-24 23:56:40 +0700

    update file

---
+++ Sliding navbar/script.js
@@ -0,0 +1,14 @@
+let slider = document.getElementById("slider");
+
+function getNthChils(element) {
+    let parent = element.parentNode;
+    let children = Array.from(parent.children);
+    return children.indexOf(element);
+}
+document.querySelectorAll(".navbar > span").forEach((element) => {
+    span.addEventListener("mouseover", (element) => {
+        let nthChild = getNthChild(element.target);
+        
+    })
+})
+    
