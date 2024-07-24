b4ddb8f8f96e48992c545350fa605664b7352448
jonell888 <643070310021@npu.ac.th>
2024-07-24 23:59:24 +0700

    new

--- Sliding navbar/script.js
+++ Sliding navbar/script.js
@@ -1,11 +1,11 @@
 let slider = document.getElementById("slider");
 
-function getNthChils(element) {
+function getNthChild(element) {
     let parent = element.parentNode;
     let children = Array.from(parent.children);
     return children.indexOf(element);
 }
-document.querySelectorAll(".navbar > span").forEach((element) => {
+document.querySelectorAll(".navbar > span").forEach((span) => {
     span.addEventListener("mouseover", (element) => {
         let nthChild = getNthChild(element.target);
         slider.style.setProperty("--value", nthChild);
